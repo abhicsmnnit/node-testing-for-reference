8c808a704609c6619f017fc2d0fbfeeee11103c0
Abhinav Verma <abhicsmnnit@gmail.com>
2018-01-05 17:29:44 +0530

    Using Assertion Library Expect to make nice assertions

--- utils/utils.js
+++ utils/utils.js
@@ -1,3 +1,10 @@
 module.exports.add = (a, b) => a + b;
 
 module.exports.square = (x) => x * x;
+
+module.exports.setName = (user, fullName) => {
+    const name = fullName.split(' ');
+    user.firstname = name[0];
+    user.lastname = name[1];
+    return user;
+};
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,17 +1,24 @@
-var utils = require('./utils');
+const expect = require('expect');
+
+const utils = require('./utils');
 
 it('should add two numbers', () => {
     const res = utils.add(9, 15);
 
-    if (res !== 24) {
-        throw new Error(`Expected 24, but got ${res}.`);
-    }
+    expect(res).toBe(24).toBeA('number');
 });
 
 it('should square a number', () => {
     const res = utils.square(5);
 
-    if (res !== 25) {
-        throw new Error(`Expected 25, but got ${res}`);
-    }
+    expect(res).toBe(25).toBeA('number');
+});
+
+it('should set firstname and lastname with proper values', () => {
+    const userWithName = utils.setName({}, 'Hare Krishna');
+
+    expect(userWithName).toInclude({
+        firstname: 'Hare',
+        lastname: 'Krishna'
+    });
 });
