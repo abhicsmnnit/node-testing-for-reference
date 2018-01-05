46578d63a9fe2f74b82f2cfae9de6b503e39d005
Abhinav Verma <abhicsmnnit@gmail.com>
2018-01-05 20:52:53 +0530

    Organized the test cases with mocha's describe()

--- server/server.test.js
+++ server/server.test.js
@@ -3,38 +3,46 @@ const expect = require('expect');
 
 const app = require('./server').app;
 
-// Normal Text Test Case
-it('should return Hare Krishna! on /', (done) => {
-    request(app)
-        .get('/')
-        .expect(200)
-        .expect('Hare Krishna!')
-        .end(done);
-});
-
-// Json Test Case
-it('should return 400 "Page not found." error json on /error', (done) => {
-    request(app)
-        .get('/error')
-        .expect(404)
-        .expect((res) => {
-            expect(res.body).toContain({
-                error: 'Page not found.'
-            });
-        })
-        .end(done);
-});
-
-// Json Array Test Case
-it('should contain me on /users', (done) => {
-    request(app)
-        .get('/users')
-        .expect(200)
-        .expect((res) => {
-            expect(res.body).toContain({
-                name: 'Abhinav',
-                age: 27
-            });
-        })
-        .end(done);
+describe('Server', () => {
+    // Normal Text Test Case
+    describe('GET /', () => {
+        it('should return Hare Krishna! on /', (done) => {
+            request(app)
+                .get('/')
+                .expect(200)
+                .expect('Hare Krishna!')
+                .end(done);
+        });
+    });
+    
+    // Json Test Case
+    describe('GET /error', () => {
+        it('should return 400 "Page not found." error json on /error', (done) => {
+            request(app)
+                .get('/error')
+                .expect(404)
+                .expect((res) => {
+                    expect(res.body).toContain({
+                        error: 'Page not found.'
+                    });
+                })
+                .end(done);
+        });
+    });
+    
+    // Json Array Test Case
+    describe('GET /users', () => {
+        it('should contain me on /users', (done) => {
+            request(app)
+                .get('/users')
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body).toContain({
+                        name: 'Abhinav',
+                        age: 27
+                    });
+                })
+                .end(done);
+        });
+    });
 });
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -2,41 +2,47 @@ const expect = require('expect');
 
 const utils = require('./utils');
 
-it('should add two numbers', () => {
-    const res = utils.add(9, 15);
+describe('Utils', () => {
+    describe('#add #square', () => {
+        it('should add two numbers', () => {
+            const res = utils.add(9, 15);
 
-    expect(res).toBe(24).toBeA('number');
-    // toBe() also checks if 24 is a number as it uses === to check for equality
-    // using toBeA('number') just to showcase how to chain these methods
-});
+            expect(res).toBe(24).toBeA('number');
+            // toBe() also checks if 24 is a number as it uses === to check for equality
+            // using toBeA('number') just to showcase how to chain these methods
+        });
 
-it('should square a number', () => {
-    const res = utils.square(5);
+        it('should square a number', () => {
+            const res = utils.square(5);
 
-    expect(res).toBe(25).toBeA('number');
-});
-
-it('should set firstname and lastname with proper values', () => {
-    const userWithName = utils.setName({}, 'Hare Krishna');
-
-    expect(userWithName).toInclude({
-        firstname: 'Hare',
-        lastname: 'Krishna'
+            expect(res).toBe(25).toBeA('number');
+        });
     });
-});
 
-// Async Test Cases
-// Don't forget to use the 'done' parameter and then call it when async call returns
-it('should async-add 2 numbers', (done) => {
-    utils.asyncAdd(10, 20, (sum) => {
-        expect(sum).toBe(30).toBeA('number');
-        done();
+    it('should set firstname and lastname with proper values', () => {
+        const userWithName = utils.setName({}, 'Hare Krishna');
+
+        expect(userWithName).toInclude({
+            firstname: 'Hare',
+            lastname: 'Krishna'
+        });
     });
-});
 
-it ('should async-square a number', (done) => {
-    utils.asyncSquare(10, (square) => {
-        expect(square).toBe(100);
-        done();
+    // Async Test Cases
+    // Don't forget to use the 'done' parameter and then call it when async call returns
+    describe('Async versions of #add #square', () => {
+        it('should async-add 2 numbers', (done) => {
+            utils.asyncAdd(10, 20, (sum) => {
+                expect(sum).toBe(30).toBeA('number');
+                done();
+            });
+        });
+
+        it('should async-square a number', (done) => {
+            utils.asyncSquare(10, (square) => {
+                expect(square).toBe(100);
+                done();
+            });
+        });
     });
 });
