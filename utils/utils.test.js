6135de3b95684bb4026afddb36941054c9ab6b98
Abhinav Verma <abhicsmnnit@gmail.com>
2018-01-05 18:19:53 +0530

    Added test cases for testing async code

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -6,6 +6,8 @@ it('should add two numbers', () => {
     const res = utils.add(9, 15);
 
     expect(res).toBe(24).toBeA('number');
+    // toBe() also checks if 24 is a number as it uses === to check for equality
+    // using toBeA('number') just to showcase how to chain these methods
 });
 
 it('should square a number', () => {
@@ -20,5 +22,21 @@ it('should set firstname and lastname with proper values', () => {
     expect(userWithName).toInclude({
         firstname: 'Hare',
         lastname: 'Krishna'
+    });
+});
+
+// Async Test Cases
+// Don't forget to use the 'done' parameter and then call it when async call returns
+it('should async-add 2 numbers', (done) => {
+    utils.asyncAdd(10, 20, (sum) => {
+        expect(sum).toBe(30).toBeA('number');
+        done();
+    });
+});
+
+it ('should async-square a number', (done) => {
+    utils.asyncSquare(10, (square) => {
+        expect(square).toBe(100);
+        done();
     });
 });
